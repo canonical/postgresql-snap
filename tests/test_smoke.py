d02dccc376d906d0c90a2dc8f6a326bc9ab6eb1b
Dragomir Penev <6687393+dragomirp@users.noreply.github.com>
2025-02-28 18:54:21 +0200

    [DPE-6749] Initial standalone snap (#1)
    * Initial standalone snap
    
    * Removed charmed reference from readme
    
    * Jira components
    
    * Bump lock format
    
    * Wrong link
    
    * Fix license location
    
    * Third time's the charm
    
    * Cleanup tests
    
    * Bump dpw
    
    * Update snap/local/start-postgresql.sh
    
    Co-authored-by: Alex Lutay <1928266+taurus-forever@users.noreply.github.com>
    
    * Add custom marker
    
    * Don't print command output
    
    * Check output tests
    
    * Remove cfg hook
    
    ---------
    
    Co-authored-by: Alex Lutay <1928266+taurus-forever@users.noreply.github.com>

---
+++ tests/test_smoke.py
@@ -0,0 +1,89 @@
+import yaml
+import subprocess
+import time
+import pytest
+
+
+def test_install():
+    with open("snap/snapcraft.yaml") as file:
+        snapcraft = yaml.safe_load(file)
+
+        subprocess.run(
+            f"sudo snap install ./{snapcraft['name']}_{snapcraft['version']}_amd64.snap --devmode".split(),
+            check=True,
+        )
+
+
+@pytest.mark.run(after="test_install")
+def test_all_apps():
+    with open("snap/snapcraft.yaml") as file:
+        snapcraft = yaml.safe_load(file)
+
+        override = {}
+
+        skip = [
+            "pg-buildext",
+            "pg-conftool",
+            "pg-createcluster",
+            "pg-dropcluster",
+            "pg-upgradecluster",
+            "pg-backupcluster",
+            "pg-restorecluster",
+            "pg-virtualenv",
+            "pg-ctlcluster",
+            "pg-renamecluster",
+            "pg-updatedicts",
+            "pg-lsclusters",
+        ]
+
+        for app, data in snapcraft["apps"].items():
+            if not bool(data.get("daemon")) and app not in skip:
+                print(f"Running {snapcraft['name']}.{app}...")
+                try:
+                    subprocess.check_output(
+                        f"{snapcraft['name']}.{app} {override.get(app, '--help')}".split()
+                    )
+                except subprocess.CalledProcessError as e:
+                    print(e)
+                    raise e
+
+
+@pytest.mark.run(after="test_install")
+def test_all_services():
+    with open("snap/snapcraft.yaml") as file:
+        snapcraft = yaml.safe_load(file)
+
+        skip = []
+
+        for app, data in snapcraft["apps"].items():
+            if bool(data.get("daemon")) and app not in skip:
+                print(f"Running {snapcraft['name']}.{app}...")
+                try:
+                    subprocess.check_output(
+                        f"sudo snap start {snapcraft['name']}.{app}".split()
+                    )
+                    time.sleep(5)
+                    service = subprocess.check_output(
+                        f"snap services {snapcraft['name']}.{app}".split()
+                    )
+                    subprocess.check_output(
+                        f"sudo snap stop {snapcraft['name']}.{app}".split()
+                    )
+
+                    assert "active" in service.decode()
+                except subprocess.CalledProcessError as e:
+                    print(e)
+                    raise e
+
+
+@pytest.mark.run(after="test_install")
+def test_version():
+    with open("snap/snapcraft.yaml") as file:
+        snapcraft = yaml.safe_load(file)
+        snap_version = snapcraft["version"]
+        app_version = (
+            subprocess.check_output([f"{snapcraft['name']}.pg-isready", "--version"])
+            .decode()
+            .split(" ")[2]
+        )
+        assert snap_version == app_version
