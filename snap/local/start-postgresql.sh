e86d2cf8a5d3348ffba36657023455142626e3c3
Dragomir Penev <dragomir.penev@canonical.com>
2025-04-03 17:59:01 +0300

    Twea install

--- snap/local/start-postgresql.sh
+++ snap/local/start-postgresql.sh
@@ -3,5 +3,6 @@
 # For security measures, applications should not be run as sudo.
 export LOCPATH="${SNAP}"/usr/lib/locale
 export PGDATA=${SNAP_COMMON}/pgsql/data
+PGMAJORVER=$(echo "$SNAP_VERSION" | cut -d "." -f 1)
 
-"${SNAP}/usr/bin/setpriv" --clear-groups --reuid snap_daemon --regid snap_daemon -- "${SNAP}/usr/lib/postgresql/16/bin/postgres" -k /tmp -D "${PGDATA}"
+"${SNAP}/usr/bin/setpriv" --clear-groups --reuid snap_daemon --regid snap_daemon -- "${SNAP}/usr/lib/postgresql/${PGMAJORVER}/bin/postgres" -k /tmp -D "${PGDATA}"
