71947aaf15edbd10c211eab9019eb695e0d7f745
Dragomir Penev <dragomir.penev@canonical.com>
2025-04-03 20:36:47 +0300

    Fake clustering

--- snap/local/start-postgresql.sh
+++ snap/local/start-postgresql.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # For security measures, applications should not be run as sudo.
-export LOCPATH="${SNAP}"/usr/lib/locale
+export LOCPATH="$SNAP"/usr/lib/locale
 export PGDATA=${SNAP_COMMON}/pgsql/data
-PGMAJORVER=$(echo "$SNAP_VERSION" | cut -d "." -f 1)
+PG_MAJOR_VER=$(echo "$SNAP_VERSION" | cut -d "." -f 1)
+export CLUSTERCONF="$SNAP_DATA/etc/postgresql/$PG_MAJOR_VER/main"
 
-"${SNAP}/usr/bin/setpriv" --clear-groups --reuid snap_daemon --regid snap_daemon -- "${SNAP}/usr/lib/postgresql/${PGMAJORVER}/bin/postgres" -k /tmp -D "${PGDATA}"
+"$SNAP/usr/bin/setpriv" --clear-groups --reuid snap_daemon --regid snap_daemon -- "$SNAP/usr/lib/postgresql/$PG_MAJOR_VER/bin/postgres" -k /tmp -D "$PGDATA" -c config_file="$CLUSTERCONF/postgresql.conf" -c hba_file="$CLUSTERCONF/pg_hba.conf" -c ident_file="$CLUSTERCONF/pg_ident.conf"
