76ca63b6a82a39c1d0a03caabfc07933867b0ae4
Carl Csaposs <carl.csaposs@canonical.com>
2026-09-10 13:20:53 +0000

    Don't expose cluster wrappers (#204)
    Per request of @taurus-forever

--- snap/local/rename-cluster.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-if [ "${EUID}" != "0" ]; then
-  echo "Error: run it as root (to utilize snap user _daemon_)." >&2
-  exit 1
-fi
-
-"$SNAP/usr/bin/setpriv" --clear-groups --reuid _daemon_ --regid root -- pg_renamecluster "$@"
